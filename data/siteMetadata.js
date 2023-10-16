84cc6fb835c7add0c688137d1ff0882249182115
hankx <hank@momane.com>
2023-10-16 15:36:14 +0800

    clean some code

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,23 +1,19 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Hank\'s Blog',
+  author: 'Hank',
+  headerTitle: 'Hank ',
+  description: 'Hank\'s Blog, JavaScript, React, Vue, Python, Low Code, Productivity, and more.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
-  socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  siteUrl: 'https://momane.com',
+  siteRepo: 'https://github.com/hankxdev/tailwind-nextjs-blog',
+  siteLogo: '/static/favicon/site-logo.png',
+  email: 'hank@momane.com',
+  github: 'https://github.com/hankxdev',
+  linkedin: 'https://www.linkedin.com/in/hank-x',
   locale: 'en-US',
+  socialBanner: '/static/images/twitter-card.png',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
